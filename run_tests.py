6aa70a7d6c4da94d0f2f47c9019e81d22695b5d1
jamalshabani <jjuma1992@gmail.com>
2022-04-02 13:16:57 -0400

    Gap optimization tester

--- run_tests.py
+++ run_tests.py
@@ -12,7 +12,7 @@ import subprocess
 
 # Ratio Er/Es = 0.1
 program_list = ["python3 2_mat_res_prob.py -tao_monitor -tao_max_it 200 -l -0.005 -m '1_to_1_mesh.msh' -o '1_to_1_mesh' -er 1.0e-1 -es 1.0 -k 1.0e-5 -e 4.0e-3 -p 1.0",
-                #"python3 2_mat_res_prob.py -tao_monitor -tao_max_it 200 -l -0.0001 -m '1_to_3_mesh.msh' -o '1_to_3_mesh' -er 1.0e-1 -es 1.0 -k 1.0e-5 -e 4.0e-3 -p 1.0",
+                "python3 2_mat_res_prob.py -tao_monitor -tao_max_it 200 -l -0.0001 -m '1_to_3_mesh.msh' -o '1_to_3_mesh' -er 1.0e-1 -es 1.0 -k 1.0e-5 -e 4.0e-3 -p 1.0",
                 "python3 2_mat_res_prob.py -tao_monitor -tao_max_it 200 -l -0.01 -m '1_to_6_mesh.msh' -o '1_to_6_mesh' -er 1.0e-1 -es 1.0 -k 1.0e-5 -e 4.0e-3 -p 1.0"]
 
 i = 1
