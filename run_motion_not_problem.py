b317e772b94a3cb4eb56fc2df7606523d49fa047
jamalshabani <jjuma1992@gmail.com>
2022-10-31 17:36:43 -0400

    no traction force

---
+++ run_motion_not_problem.py
@@ -0,0 +1,20 @@
+import subprocess
+
+
+# Ratio Er/Es = 10.0
+program_list = ["rm -rf motion_test1_kI", "rm -rf motion_test2_kI", "rm -rf motion_test3_kI",
+                "python3 motion_nof_problem.py -tao_type blmvm -tao_monitor -tao_max_it 2000 -tao_gatol 1e-7 -lr 0.19 -ls 0.3 -m 'motion_nof_mesh1.msh' -o 'motion_nof1' -er 1.0 -es 1.0e-2 -k 1.7e-3 -e 4.0e-3 -p 2.0 -s 1.0",
+                "python3 motion_nof_problem.py -tao_type blmvm -tao_monitor -tao_max_it 2000 -tao_gatol 1e-7 -lr 0.19 -ls 0.35 -m 'motion_nof_mesh1.msh' -o 'motion_nof2' -er 1.0 -es 1.0e-2 -k 1.7e-3 -e 4.0e-3 -p 2.0 -s 1.0",
+                "python3 motion_nof_problem.py -tao_type blmvm -tao_monitor -tao_max_it 2000 -tao_gatol 1e-7 -lr 0.19 -ls 0.4 -m 'motion_nof_mesh1.msh' -o 'motion_nof3' -er 1.0 -es 1.0e-2 -k 1.7e-3 -e 4.0e-3 -p 2.0 -s 1.0"]
+
+
+i = 1
+for program in program_list:
+    print("------------------------------------------------------------------------------")
+    print("")
+    print("Running test #{}".format(i))
+    print("")
+    print(program)
+    print("")
+    subprocess.run(program, shell = True)
+    i = i + 1
